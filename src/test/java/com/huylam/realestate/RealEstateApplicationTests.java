146f8beb348c8016cafb4f71012618823e956f10
Huy Lam <huy.lam0199@gmail.com>
2023-07-04 11:54:45 +0700

    first commit

---
+++ src/test/java/com/huylam/realestate/RealEstateApplicationTests.java
@@ -0,0 +1,11 @@
+package com.huylam.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
